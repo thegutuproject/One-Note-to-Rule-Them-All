ad54adb93152de115a3bce8160384c72aef1e587
Alexandru Gutu <alex.s.gutu@gmail.com>
2019-08-30 19:51:22 -0400

    Knex stuff

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,38 @@ module.exports = {
     client: 'mysql',
     useNullAsDefault: true,
     connection: {
+      host : process.env.LOCAL_DB_HOST,
+      user : process.env.LOCAL_DB_USER,
+      password : process.env.LOCAL_DB_PASSWORD,
+      database : process.env.LOCAL_DB_DATABASE,
+      charset : 'utf8mb4'
+    },
+    migrations: {
+
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    log: {
+      warn(message) {
+        console.log(message);
+      },
+      error(message) {
+        console.log(message);
+      },
+      deprecate(message) {
+        console.log(message);
+      },
+      debug(message) {
+        console.log(message);
+      },
+    }
+  },
+  production: {
+    client: 'mysql',
+    useNullAsDefault: true,
+    connection: {
       host : process.env.DB_HOST,
       user : process.env.DB_USER,
       password : process.env.DB_PASSWORD,
